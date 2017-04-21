d7ba12e948bc818e313ea9769382fca25a10f19f
Ponomarenko <albka1986@gmail.com>
2017-04-22 02:10:14 +0300

    updated;

--- app/src/main/java/com/ponomarenko/myipadrress/UI/activity/ui/MainActivity.java
+++ app/src/main/java/com/ponomarenko/myipadrress/UI/activity/ui/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.ponomarenko.myipadrress.R;
 import com.ponomarenko.myipadrress.UI.activity.utils.Utils;
@@ -16,7 +19,7 @@ import com.ponomarenko.myipadrress.UI.activity.utils.Utils;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
+    private AdView mAdView;
     private TextView ipAddressTextView, networkNameTextView, networkTypeTexView;
     private FirebaseAnalytics mFirebaseAnalytics;
     private View.OnClickListener clickListener;
@@ -27,14 +30,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle(getString(R.string.title_main_screen));
         initializeViews();
+        loadAdvertisement();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-       /* mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "bundle_id");
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "bundle_name");
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);*/
 
+    }
+
+    private void loadAdvertisement() {
+        MobileAds.initialize(getApplicationContext(), getString(R.string.banner_ad_unit_id));
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     private void initializeViews() {
@@ -100,12 +106,21 @@ public class MainActivity extends AppCompatActivity {
     private class MyOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+            Bundle bundle = new Bundle();
+            bundle.putInt(FirebaseAnalytics.Param.ITEM_ID, v.getId());
+
 
             switch (v.getId()) {
                 case R.id.refresh_button:
                     loadData();
+                    bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Refreshed IP ADDRESS");
+                    mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
                     break;
                 default:
+                    bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Copy data to clipboard");
+                    mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
                     CharSequence text = ((TextView) v).getText();
                     copyToClipboard("MyIpAddress", text);
                     break;
