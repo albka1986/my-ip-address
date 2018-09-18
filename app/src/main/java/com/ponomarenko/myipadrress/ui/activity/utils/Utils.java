bb26e0c50d6beeb4f265330ff15d85bb5ba2d2fb
Oleg Ponomarenko <oleg.ponomarenko@flipsidegroup.com>
2018-09-18 15:01:21 +0300

    gradle was updated

--- app/src/main/java/com/ponomarenko/myipadrress/ui/activity/utils/Utils.java
+++ app/src/main/java/com/ponomarenko/myipadrress/ui/activity/utils/Utils.java
@@ -24,14 +24,12 @@ public class Utils {
         try {
             List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
             for (NetworkInterface intf : interfaces) {
-                if (interfaceName != null) {
-                    if (!intf.getName().equalsIgnoreCase(interfaceName)) continue;
-                }
+                if (interfaceName != null && !intf.getName().equalsIgnoreCase(interfaceName))
+                    continue;
                 byte[] mac = intf.getHardwareAddress();
                 if (mac == null) return "";
                 StringBuilder buf = new StringBuilder();
-                for (int idx = 0; idx < mac.length; idx++)
-                    buf.append(String.format("%02X:", mac[idx]));
+                for (byte aMac : mac) buf.append(String.format("%02X:", aMac));
                 if (buf.length() > 0) buf.deleteCharAt(buf.length() - 1);
                 return buf.toString();
             }
