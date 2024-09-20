9eb0b51178428018cc3758ac26acba6306c6ac64
Oleh Ponomarenko <oleh@excelsior-solutions.de>
2024-09-20 20:30:43 +0200

    - updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 12
-        versionName = "2.0"
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
